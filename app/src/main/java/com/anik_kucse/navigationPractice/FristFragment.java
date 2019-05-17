cdfd6955482b59110dbc005e8ff55cea8fc1d86c
J. M. Ashfiqur Rahman <anik.ku.cse@gmail.com>
2019-05-17 12:50:09 +0600

    Navigation First commit. New Things learned: app:popUpTo app:popUpToInclusive

---
+++ app/src/main/java/com/anik_kucse/navigationPractice/FristFragment.java
@@ -0,0 +1,43 @@
+package com.anik_kucse.navigationPractice;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FristFragment extends Fragment {
+
+    private Button button;
+
+    public FristFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frist, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = view.findViewById(R.id.button_first_fragment);
+        button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_fristFragment_to_secondFragment));
+    }
+}
---
+++ app/src/main/java/com/anik_kucse/navigationPractice/SecondFragment.java
@@ -0,0 +1,42 @@
+package com.anik_kucse.navigationPractice;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SecondFragment extends Fragment {
+
+
+    public SecondFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button button = view.findViewById(R.id.button_second_fragment);
+        button.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_secondFragment_to_thirdFragment));
+    }
+}
